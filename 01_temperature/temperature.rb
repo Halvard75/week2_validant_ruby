5e0ad13150f96af0c1b4bec718606d4e4eec8632
Halvard75 <metivier.anto@gmail.com>
2017-10-10 20:03:50 +0200

    2_eme_sauvegarde

--- 01_temperature/temperature.rb
+++ 01_temperature/temperature.rb
@@ -1,1 +1,24 @@
-#write your code here
+def ftoc (temp_f)
+  temp = 0.0
+
+  if temp_f.is_a? Float
+	temp = temp_f 
+  else
+  	temp = temp_f.to_f
+  end
+    
+  temp_c = (temp - 32.0) * 5 / 9
+
+end
+def ctof(temp_c)
+
+  temp = 0.0
+
+  if temp_c.is_a? Float
+	temp = temp_c
+  else
+  	temp = temp_c.to_f
+  end
+  temp_f = (temp * 9 / 5) + 32.0
+
+end
